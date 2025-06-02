9f9f7b5477833ff77c5c8a18f204287ad2068362
AdlinHFDZ <adlinhfdz@gmail.com>
2025-06-03 00:21:28 +0800

    bug update

--- resources/views/SEMS/create-question.blade.php
+++ resources/views/SEMS/create-question.blade.php
@@ -402,13 +402,10 @@ function initAllTinyMCE() {
   });
   tinymce.init({
     selector: 'textarea.tinymce',
-    plugins: [
-      'anchor', 'autolink', 'charmap', 'codesample', 'emoticons', 'image', 'link', 'lists', 'media', 'searchreplace',
-      'table', 'visualblocks', 'wordcount', 'checklist', 'mediaembed', 'casechange', 'formatpainter', 'pageembed',
-      'a11ychecker', 'tinymcespellchecker', 'permanentpen', 'powerpaste', 'advtable', 'advcode', 'editimage',
-      'advtemplate', 'ai', 'mentions', 'tinycomments', 'tableofcontents', 'footnotes', 'mergetags', 'autocorrect',
-      'typography', 'inlinecss', 'markdown','importword', 'exportword', 'exportpdf'
-    ],
+ plugins: [
+  'anchor', 'autolink', 'charmap', 'codesample', 'emoticons', 'image', 'link', 'lists', 'media', 'searchreplace',
+  'table', 'visualblocks', 'wordcount'
+],
     toolbar: 'undo redo | blocks fontfamily fontsize | bold italic underline strikethrough | link image media table mergetags | addcomment showcomments | spellcheckdialog a11ycheck typography | align lineheight | checklist numlist bullist indent outdent | emoticons charmap | removeformat',
     tinycomments_mode: 'embedded',
     tinycomments_author: @json(Auth::user()->name ?? "Author"),
